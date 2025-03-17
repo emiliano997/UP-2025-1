361c833a2c5f7f63a8dd79a276ecca77200805a5
emilianoperez <emi.perez997@gmail.com>
2025-03-17 15:22:52 -0300

    [ADD] Clase 1

---
+++ clase_1/js/tiposDeDatos.js
@@ -0,0 +1,33 @@
+// Tipos de datos
+// String -> Texto
+let nombre = "Emiliano";
+let apellido = "Garcia";
+let direccion = "Calle falsa 123";
+let localidad = "CABA";
+let pais = "Argentina";
+let telefono = "+541123456789";
+
+// Number -> Números
+let edad = 30;
+let altura = 1.75;
+let peso = 63.87;
+let dni = 40123456;
+let birthYear = 1990;
+
+// Boolean -> Verdadero o Falso
+let esEstudiante = true;
+let esCasado = false;
+let esMayorDeEdad = true;
+
+// Null -> Valor null
+let casaPropia = null;
+
+// Undefined -> Valor no definido
+let nombreUsuario; // undefined
+nombreUsuario = "emipe";
+
+// ParseInt y Number
+let numero = parseInt("23"); // Transforma el string a un number
+let numero2 = Number("453"); // Transforma el string a un number
+
+typeof nombre;
---
+++ clase_1/js/variables.js
@@ -0,0 +1,23 @@
+// Variables y Constantes
+
+// Variables -> let
+// let <nombreVariable> = <valor>;
+let nombre = "Juan";
+let apellido = "Perez";
+let edad = 30;
+// edad = "30 años"; // ❌
+
+// Reasignar valor
+nombre = "Emiliano";
+
+// Ejemplo
+let username = ""; // No se que nombre tiene el usuario
+username = "juanperez"; // El nombre que ingresa el usuario
+
+// Constantes -> const
+// const <nombreConstante> = <valor>;
+const nombreCompleto = "Juan Perez";
+// nombreCompleto = "Emiliano"; // ❌
+const PI = 3.1416;
+
+let resultado = PI * 5; // Ejemplo de uso
